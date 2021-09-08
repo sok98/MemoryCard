60ba127c97529c10630c8abfa4ed07bb60c659c5
SeoYeseul <sok98@naver.com>
2021-09-08 17:03:15 +0900

    Feat: card ClickListener

--- app/src/main/java/com/yeseul/memorycard/adapter/CardAdapter.kt
+++ app/src/main/java/com/yeseul/memorycard/adapter/CardAdapter.kt
@@ -1,19 +1,49 @@
 package com.yeseul.memorycard.adapter
 
+import android.graphics.Color
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
+import com.yeseul.memorycard.R
 import com.yeseul.memorycard.databinding.ItemWordCardBinding
 import com.yeseul.memorycard.data.db.entity.Word
 
 class CardAdapter : ListAdapter<Word, CardAdapter.ViewHolder>(diffUtil) {
 
+    private var tag = true
+
     inner class ViewHolder(private val binding: ItemWordCardBinding): RecyclerView.ViewHolder(binding.root) {
         fun bind(word: Word) {
             binding.wordTextView.text = word.word
+            initWordTextView(binding)
+
+            binding.root.setOnClickListener {
+                if (tag){
+                    binding.wordTextView.text = word.meaning
+                    meaningWordTextView(binding)
+
+                } else {
+                    binding.wordTextView.text = word.word
+                    initWordTextView(binding)
+
+                }
+            }
         }
+    }
+
+    private fun initWordTextView(binding: ItemWordCardBinding) {
+        binding.wordTextView.setTextColor(Color.BLACK)
+        binding.wordLinearLayout.setBackgroundResource(R.drawable.background_card)
+        tag = true
+
+    }
+
+    private fun meaningWordTextView(binding: ItemWordCardBinding) {
+        binding.wordTextView.setTextColor(Color.WHITE)
+        binding.wordLinearLayout.setBackgroundResource(R.drawable.background_card_meaning)
+        tag = false
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
